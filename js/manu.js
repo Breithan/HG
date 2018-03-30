7e2bd3e8a4ab00f4bae321b1e22a9f723feade96
Breithan <32719925+Breithan@users.noreply.github.com>
2018-03-30 07:49:59 -0500

    S1.9

--- js/manu.js
+++ js/manu.js
@@ -11,3 +11,22 @@ window.onload = function() {
 
     };
 };
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1250, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
+
+function aColor() {
+    $('a').css({ "border": 3 + 'px' + 'solid' + 'grey'});
+    console.log('hola');
+}
