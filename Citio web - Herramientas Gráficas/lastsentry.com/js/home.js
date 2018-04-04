b36ca76a48e22a3a742b62b20d024f096ee3ba18
Breithan <32719925+Breithan@users.noreply.github.com>
2018-04-03 21:39:51 -0500

    Entrega

---
+++ Citio web - Herramientas Gráficas/lastsentry.com/js/home.js
@@ -0,0 +1,13 @@
+window.onload = function() {
+    document.onmousemove = function(e) {
+        var x = -(e.clientX / 30);
+        var y = (e.clientY / 30);
+
+        $('.float').each(
+            function() {
+                $(this).css({"background-position": (x + y) + 'px'});
+            }
+        )
+
+    };
+};
---
+++ Citio web - Herramientas Gráficas/lastsentry.com/js/manu.js
@@ -0,0 +1,27 @@
+window.onload = function() {
+    document.onmousemove = function(e) {
+        var x = -(e.clientX / 30);
+        var y = (e.clientY / 30);
+
+        $('.float').each(
+            function() {
+                $(this).css({"right": (x + y) + 'px'});
+            }
+        )
+
+    };
+};
+
+$(document).ready(function () {
+    $("a").on('click', function (event) {
+        if (this.hash !== "") {
+            event.preventDefault();
+            var hash = this.hash;
+            $('html, body').animate({
+                scrollTop: $(hash).offset().top
+            }, 1250, function () {
+                window.location.hash = hash;
+            });
+        }
+    });
+});
